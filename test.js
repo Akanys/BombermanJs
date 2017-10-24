af8be953ac66529fe789a28cdb7beedf7d12cc6c
NinjaRetroGaming <nicola.f@codeur.online>
2017-10-24 16:24:12 +0200

    colobomb

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
-var cartesol = document.getElementById('carteSol')
-var player = document.getElementById('player')
-var bomb = document.createElement('div')
-bomb.style.left = player.style.left
-bomb.style.top = player.style.top
-carteSol.appendChild(bomb);
+var cartesol = document.getElementById('carteSol');
+var player = document.getElementById('player');
+
 
 document.addEventListener("keydown", function(ev)
 {
-    if (ev.keyCode == 32){ // bomb
-    	bomb ;
-    var id = "l"+x+"-c"+y;
-    var deplacement = document.getElementById(id).getAttribute("data-valeur") ;
-    console.log('la bombe test '+ parseInt(deplacement));
-    console.log(id('bomb'));
+    if (ev.keyCode == 32){
+    	var bomb = document.createElement('div');
+		bomb.style.left = player.style.left ;
+		bomb.style.top = player.style.top;
+		bomb.className ="bombe";
+		carteSol.appendChild(bomb);
+		bombe.style.backgroundImage != 'url("bomb1.png")'
+		console.log("Attention y a une bombe");
+  
+	}
+});
 
-    }
