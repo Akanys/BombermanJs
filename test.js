ba250d870df92e801224acec20a22fac04545e61
RollonDaim <rdonlineacs90@gmail.com>
2017-10-24 16:14:24 +0200

    fleurs

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
-var cartesol = document.getElementById('carteSol')
-var player = document.getElementById('player')
-var bomb = document.createElement('div')
-bomb.style.left = player.style.left
-bomb.style.top = player.style.top
-carteSol.appendChild(bomb);
-
+var cartesol = document.getElementById('carteSol');
+var player = document.getElementById('player');
+var bomb = document.getElementById('bombe');
+bomb.style.left = player.style.left ;
+bomb.style.top = player.style.top ;
 document.addEventListener("keydown", function(ev)
 {
-    if (ev.keyCode == 32){ // bomb
-    	bomb ;
-    var id = "l"+x+"-c"+y;
-    var deplacement = document.getElementById(id).getAttribute("data-valeur") ;
-    console.log('la bombe test '+ parseInt(deplacement));
-    console.log(id('bomb'));
-
-    }
+   if (ev.keyCode == 32){
+   console.log("Attention y a une bombe");
+    bombe.style.visibility = 'visible';
+    cartesol.appendChild(bomb);
+}
+});
